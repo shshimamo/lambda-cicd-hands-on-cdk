561ea586ab9531baf7a91a740c1ec04ca7dd221f
hiroya.shimamoto <shshimamo@gmail.com>
2023-01-02 01:39:02 +0900

    cdk init

---
+++ test/lambda-cicd-hands-on-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaCicdHandsOnCdk from '../lib/lambda-cicd-hands-on-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-cicd-hands-on-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaCicdHandsOnCdk.LambdaCicdHandsOnCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
